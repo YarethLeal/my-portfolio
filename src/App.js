6c10f70f5d48815b554a6e0a377fd32df407a511
YarethLeal <yarethleal65@gmail.com>
2024-06-12 21:16:36 -0600

    Initial structure and navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { useState } from 'react';
+
+// Define your supported languages
+const supportedLanguages = ['en', 'es'];
+
 
 function App() {
+  // Use React state to store the current language
+  const [currentLanguage, setCurrentLanguage] = useState(localStorage.getItem('language') || 'en');
+
+  const toggleLanguage = (selectedLanguage) => {
+    // Validate the selected language
+    if (!supportedLanguages.includes(selectedLanguage)) {
+      console.error(`Unsupported language: ${selectedLanguage}`);
+      return;
+    }
+
+    // Update the current language in state and local storage
+    setCurrentLanguage(selectedLanguage);
+    localStorage.setItem('language', selectedLanguage);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <Navbar onLanguageChange={toggleLanguage} language={currentLanguage} />
       </header>
-    </div>
+      <main></main>
+      <footer></footer>
+    </>
   );
 }
 
---
+++ src/components/Information.js
@@ -0,0 +1,223 @@
+const information = {
+    es: {
+        navbar: {
+            home: 'Inicio',
+            sobreMi: 'Sobre Mí',
+            skills: 'Skills',
+            proyectos: 'Proyectos',
+            contacto: 'Contacto',
+        },
+        home: {
+            title: 'Mi Portafolio',
+            description: 'Bienvenido a mi portafolio animado. Aquí puedes conocer más sobre mí y mis proyectos.',
+        },
+        aboutMe: {
+            title: 'Sobre Mí',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...',
+        },
+        skills: {
+            title: 'Skills',
+            content: 'Mis habilidades incluyen...',
+            badges: [
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/html-5--v1.png',
+                    Alt: 'Html Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/css3.png',
+                    Alt: 'CSS Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/javascript--v1.png',
+                    Alt: 'JavaScript Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/react-native.png',
+                    Alt: 'React Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/bootstrap.png',
+                    Alt: 'Boostrap Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/angularjs.png',
+                    Angular: 'Angular Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/python--v1.png',
+                    Alt: 'Python Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/php.png',
+                    Alt: 'PHP Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/c-sharp-logo.png',
+                    Alt: 'C# Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/net-framework.png',
+                    Alt: '.Net Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/c-sharp-logo.png',
+                    Alt: 'ASP.NET Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/mysql-logo.png',
+                    Alt: 'MySQL Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/postgreesql.png',
+                    Alt: 'PostgreSQL Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/microsoft-sql-server.png',
+                    Alt: 'Microsoft SQL Server Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/git.png',
+                    Alt: 'Git Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/github.png',
+                    Alt: 'GitHub Icon',
+                },
+            ],
+        },
+        projects: {
+            title: 'Proyectos',
+            content: 'Algunos de mis proyectos son...',
+            project: [
+                {
+                    name: 'Proyecto 1',
+                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...',
+                    image: 'https://picsum.photos/200/300',
+                    github: '',
+                },
+                {
+                    name: 'Proyecto 2',
+                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...',
+                    image: 'https://picsum.photos/200/300',
+                    github: '',
+                },
+            ],
+        },
+        contacto: {
+            title: 'Contacto',
+            content: 'Puedes contactarme en...',
+        },
+    },
+    en: {
+        navbar: {
+            home: 'Home',
+            sobreMi: 'About Me',
+            skills: 'Skills',
+            proyectos: 'Projects',
+            contacto: 'Contact',
+            cambiarIdioma: 'Languaje',
+        },
+        home: {
+            title: 'My Portfolio',
+            description: 'Welcome to my animate portfolio. Here you can learn more about me and my projects',
+        },
+        aboutMe: {
+            title: 'About me',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...',
+        },
+        skills: {
+            title: 'Skills',
+            content: 'Mis skills include...',
+            badges: [
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/html-5--v1.png',
+                    Alt: 'Html Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/css3.png',
+                    Alt: 'CSS Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/javascript--v1.png',
+                    Alt: 'JavaScript Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/react-native.png',
+                    Alt: 'React Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/bootstrap.png',
+                    Alt: 'Boostrap Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/angularjs.png',
+                    Angular: 'Angular Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/python--v1.png',
+                    Alt: 'Python Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/php.png',
+                    Alt: 'PHP Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/c-sharp-logo.png',
+                    Alt: 'C# Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/net-framework.png',
+                    Alt: '.Net Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/c-sharp-logo.png',
+                    Alt: 'ASP.NET Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/mysql-logo.png',
+                    Alt: 'MySQL Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/postgreesql.png',
+                    Alt: 'PostgreSQL Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/microsoft-sql-server.png',
+                    Alt: 'Microsoft SQL Server Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/git.png',
+                    Alt: 'Git Icon',
+                },
+                {
+                    Src: 'https://img.icons8.com/color/96/000000/github.png',
+                    Alt: 'GitHub Icon',
+                },
+            ],
+        },
+        projects: {
+            title: 'Projects',
+            content: 'Algunos de mis proyectos son...',
+            project: [
+                {
+                    name: 'Proyecto 1',
+                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...',
+                    image: 'https://picsum.photos/200/300',
+                    github: '',
+                },
+                {
+                    name: 'Proyecto 2',
+                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. EN...',
+                    image: 'https://picsum.photos/200/300',
+                    github: '',
+                },
+            ],
+        },
+        contacto: {
+            title: 'Contact',
+            content: 'Puedes contactarme en...',
+        },
+    },
+};
+
+export default information;
